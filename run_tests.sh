98f32204e472463bfc435701294a23d3a410df32
Martin Kampas <martin.kampas@jolla.com>
2021-03-08 13:47:48 +0100

    Jenkins: Take auto configuration into use

--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-SDK_BUILD=$1
+[[ $# -eq 0 ]] || { echo "Unexpected argument: '$1'"; exit 1; }
 BUILDDATE=$(ls ~/installers/repository*|sed -e 's/.*[-]\([0-9]*\).*/\1/')
 7z x ~/installers/repository-$BUILDDATE.7z
 ln -sfT ~/targets targets
@@ -14,12 +14,8 @@ export TMPDIR=$WORKSPACE_TMP
 exec "$installer" --verbose non-interactive=1 accept-licenses=1 --platform minimal --addRepository "file://$repo/common/,file://$repo/linux-64" "\$@"
 EOF
 chmod +x SailfishSDK-installer.run
-sed -i "s/SDK_VERSION = .*/SDK_VERSION = \"$SDK_BUILD\"/g" sdk-test-suite/config.py
-LATEST_SFOS=$(ls -tr ~/emulators/|tail -n 1|sed 's/.*[-]\([0-9.]*\)[-].*/\1/')
-echo LATEST_SFOS=$LATEST_SFOS
-sed -i "s/^OS_VERSION = .*/OS_VERSION = {\"ea\": \"$LATEST_SFOS\",/g" sdk-test-suite/config.py
-sed -i "s/\"latest\": .*/\"latest\": \"$LATEST_SFOS\",/g" sdk-test-suite/config.py
 mkdir sdk-test-suite.out && cd sdk-test-suite.out
+../sdk-test-suite/configure ../SailfishSDK-installer.run
 xvfb-run --server-args="-screen 0 1024x768x24" robot --exclude interactive ../sdk-test-suite
 # clean up
 [[ -f ~/SailfishOS/SDKMaintenanceTool ]] && ~/SailfishOS/SDKMaintenanceTool --verbose non-interactive=1 --platform minimal
